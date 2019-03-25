35d7483cc53c462c90ebc038f3b5b0138475055c
Om Kelkar <neha@Tanka.local>
2019-03-24 23:58:14 -0700

    quick design changes

---
+++ js/nav.js
@@ -0,0 +1,12 @@
+function nav() {
+  return <nav id="web-nav">
+    <ul id="menu">
+      <li>
+        <a href="posts.html">Posts</a>
+      </li>
+      <li>
+        <a href="resume.html">Resume</a>
+      </li>
+    </ul>
+  </nav>;
+}
